b67afdb972d8bef41962846b2cac7f2d262ac16e
Pranay Kanwar <pranay.kanwar@gmail.com>
2013-06-28 18:22:24 +0530

    Update minor version instead of patch

--- beanstalk.h
+++ beanstalk.h
@@ -13,8 +13,8 @@
 #include <sys/fcntl.h>
 
 #define BS_MAJOR_VERSION  1
-#define BS_MINOR_VERSION  0
-#define BS_PATCH_VERSION  1
+#define BS_MINOR_VERSION  1
+#define BS_PATCH_VERSION  0
 
 #define BS_STATUS_OK             0
 #define BS_STATUS_FAIL          -1
